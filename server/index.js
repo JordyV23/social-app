bb76b6793f7ff987fbf0ad718d2a174fc47c6340
JordyV23 <jordyvargas0123@gmail.com>
2024-01-27 14:46:57 -0600

    Creacion de servidor de backend
    -Importacion de dependencias iniciales
    -Configuracion de puerto
    -Enlace a mongodb
    -Correccion de paquete instalado para conexion a mongo
    -Configuracion inicial para el manejo de archivos

--- server/index.js
+++ server/index.js
@@ -6,4 +6,44 @@ import dotenv from "dotenv";
 import multer from "multer";
 import morgan from "morgan";
 import path from "path";
-import { fileURLToPath } from "url"
+import { fileURLToPath } from "url";
+import helmet from "helmet";
+
+// Configurations
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// Configurar el almacenado de archivos
+const storage = multer.diskStorage({
+  destination: function (res, file, cb) {
+    cb(null, "public/assets");
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Conexion a Mongo
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`El servidor esta corriendo en http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => console.log(` ${error} `));
